d5683703359165543c116e7c55e679a71a7e2c74
pwalski <4924911+pwalski@users.noreply.github.com>
2024-04-05 15:49:19 +0200

    Cmds offer-template and 	est failing when no GPU on non dummy runtime. (#92)
    Co-authored-by: Przemysław Walski <pwalski@users.noreply.github.com>

--- src/offer_template.rs
+++ src/offer_template.rs
@@ -1,18 +1,12 @@
 use crate::process::RuntimeConfig;
 
 use gpu_detection::model::Gpu;
-use gpu_detection::GpuDetectionError;
+use gpu_detection::GpuDetection;
 use ya_agreement_utils::OfferTemplate;
 
-pub(crate) fn gpu_detection<CONFIG: RuntimeConfig>(config: &CONFIG) -> anyhow::Result<Option<Gpu>> {
-    match gpu_detection::GpuDetection::init() {
-        Ok(gpu_detection) => {
-            let gpu = gpu_detection.detect(config.gpu_uuid())?;
-            Ok(Some(gpu))
-        }
-        Err(GpuDetectionError::LibloadingError(_)) => Ok(None),
-        Err(e) => Err(e.into()),
-    }
+pub(crate) fn gpu_detection<CONFIG: RuntimeConfig>(config: &CONFIG) -> anyhow::Result<Gpu> {
+    let gpu_detection = GpuDetection::init()?;
+    Ok(gpu_detection.detect(config.gpu_uuid())?)
 }
 
 pub(crate) fn template<CONFIG: RuntimeConfig>(_config: &CONFIG) -> anyhow::Result<OfferTemplate> {
