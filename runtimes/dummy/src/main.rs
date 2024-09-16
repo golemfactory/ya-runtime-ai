1de37305ec013b1f9e031198f0a2f094603a9f9e
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2024-09-16 17:07:50 +0200

    Add basic http server to Dummy runtime

--- runtimes/dummy/src/main.rs
+++ runtimes/dummy/src/main.rs
@@ -1,27 +1,62 @@
 use std::env;
-use std::io;
+use std::time::Duration;
 
-fn main() {
-    let mut input = String::new();
+use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+
+#[get("/")]
+async fn index(_req: HttpRequest) -> impl Responder {
+    log::info!("Endpoint: index");
+    "Welcome!"
+}
+
+#[get("/healthcheck")]
+async fn healthcheck(_req: HttpRequest) -> impl Responder {
+    log::info!("Endpoint: healthcheck");
+    HttpResponse::Ok()
+}
+
+#[get("/shutdown")]
+async fn shutdown(_req: HttpRequest) -> impl Responder {
+    log::info!("Endpoint: shutdown");
+    HttpResponse::Ok()
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct GenerateTrafficQuery {
+    #[serde(with = "humantime_serde")]
+    pub sleep_time: Duration,
+    pub response_size: u64,
+}
+
+#[get("/generate-traffic")]
+async fn generate_traffic(info: web::Query<GenerateTrafficQuery>) -> impl Responder {
+    log::info!(
+        "Endpoint: generate-traffic. Sleep time: {}. Response size: {}",
+        humantime::format_duration(info.sleep_time),
+        info.response_size
+    );
+
+    tokio::time::sleep(info.sleep_time).await;
+    HttpResponse::Ok()
+}
+
+#[actix_web::main]
+async fn main() -> anyhow::Result<()> {
+    env_logger::init();
     let args: Vec<String> = env::args().collect();
 
-    println!("Dummy runtime. Args: {args:?}");
+    log::info!("Dummy runtime. Args: {args:?}");
 
-    loop {
-        input.clear();
-
-        match io::stdin().read_line(&mut input) {
-            Ok(_) => {
-                let trimmed_input = input.trim();
-                if trimmed_input == "stop" {
-                    println!("Stopping.");
-                    break;
-                }
-            }
-            Err(error) => {
-                eprintln!("Error reading input: {}", error);
-                // break;
-            }
-        }
-    }
+    Ok(HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(healthcheck)
+            .service(generate_traffic)
+            .service(shutdown)
+    })
+    .bind(("127.0.0.1", 7861))?
+    .run()
+    .await?)
 }
